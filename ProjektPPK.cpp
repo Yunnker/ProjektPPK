50ed159f17c757b62f4d63795a0169a9229df409
Piotr Jękner <pi.jekner@gmail.com>
2024-11-12 23:26:58 +0100

    init

---
+++ ProjektPPK.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+void HelpMessage()
+{
+    std::cout << "Sposob uzycia programu:" << std::endl
+        << "    -h Wypisuje sposob uzycia programu" << std::endl
+        << "    -i Plik wejsciowy" << std::endl
+        << "    -o Plik wyjsciowy" << std::endl
+        << "    -t Tryb: k - Kompresja, d - Dekompresja" << std::endl
+        << "    -s Plik ze slownikiem" << std::endl;
+
+
+
+
+}
+
+int main(int argc, char* argv[])
+{
+    switch (argc)
+    {
+    case 1:
+        HelpMessage();
+        return 0;
+    default:
+        if (!(argc % 2)) { HelpMessage(); return 0; }
+
+        break;
+    }
+
+    
+}
